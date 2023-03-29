4c3ff8b7a532dfdfdbdf2c46256ae37ba72985a1
Sesaque Cruz <cruz.sesaque@gmail.com>
2023-03-29 02:11:50 -0300

    Update README and Add LICENSE

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# go-openai-chatgpt
+# ChatGPT-3 CLI
+
+This is a simple command-line interface (CLI) application that uses the OpenAI API to interact with ChatGPT using the terminal.
+
+## Requirements
+
+To use this application, you will need:
+
+- Go version 1.18 or higher installed on your computer
+- An OpenAI API key (you can sign up for one [here](https://platform.openai.com/))
+- A stable internet connection
+
+## Installation
+
+To install this application: 
+
+1. Clone this repository:
+
+```
+git clone https://github.com/sesaquecruz/go-openai-chatgpt.git
+```
+
+2. Enter the project directory:
+
+```
+cd go-openai-chatgpt
+```
+
+3. Install the required packages:
+
+```
+go mod download
+```
+
+4. Set the following environment variable:
+
+```
+export OPENAI_API_KEY=<your-api-key>
+```
+
+Note: You can also set this api key in an .env file in the project directory.
+
+5. Run the program by executing:
+
+```
+go run cmd/main.go
+```
+
+## Usage
+
+After the program has been started, you can start chatting with ChatGPT. Follow the prompts in your terminal and enter your messages when prompted. ChatGPT will respond with text based on the previous messages in the conversation.
+
+## Contributing
+
+Contributions are welcome! If you find a bug or would like to suggest an enhancement, please create a new issue. If you would like to contribute code, please fork the repository and submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See LICENSE file for more information.
--- cmd/main.go
+++ cmd/main.go
@@ -13,30 +13,34 @@ import (
 	"github.com/sesaquecruz/go-openai-chatgpt/internal"
 )
 
-func menu() (int, error) {
-	fmt.Println("\nSelect an option:")
-	fmt.Println("[1] Batch mode")
-	fmt.Println("[2] Stream mode")
-	fmt.Print("\n[?] ")
+func menu() int {
+	for {
+		fmt.Println("\n[Press 'ctrl + c' to exit]")
+		fmt.Println("\nSelect an option:")
+		fmt.Println("[1] Stream mode")
+		fmt.Println("[2] Batch mode")
 
-	var option int
-	_, err := fmt.Scan(&option)
-	if err != nil || option < 1 || option > 2 {
-		return 0, err
+		fmt.Print("\n[?] ")
+		var option int
+		_, err := fmt.Scan(&option)
+		if err != nil {
+			continue
+		}
+
+		if option != 1 && option != 2 {
+			continue
+		}
+
+		return option
 	}
-
-	return option, nil
 }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalln("the .env file was not found")
-	}
+	godotenv.Load()
 
-	apiKey, ok := os.LookupEnv("API_KEY")
+	apiKey, ok := os.LookupEnv("OPENAI_API_KEY")
 	if !ok {
-		log.Fatalln("the API_KEY was not found in the .env file")
+		log.Fatalln("the API_KEY was not found")
 	}
 
 	ctx := context.Background()
@@ -44,14 +48,10 @@ func main() {
 	writer := bufio.NewWriter(os.Stdout)
 	chatGpt3 := external.NewChatGpt3(openai.NewClient(apiKey))
 
-	option, err := menu()
-	if err != nil {
-		log.Fatalln("invalid option")
-	}
-
+	option := menu()
 	if option == 1 {
-		internal.StartChatBatch(ctx, reader, writer, chatGpt3)
-	} else {
 		internal.StartChatStream(ctx, reader, writer, chatGpt3)
+	} else {
+		internal.StartChatBatch(ctx, reader, writer, chatGpt3)
 	}
 }
